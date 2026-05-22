bd62c2b6c8101369e1e45285fe8ae4834daf1127
Clark <pasambaclark@gmail.com>
2026-05-22 19:17:59 +0800

    limited results to 6 decimal places

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function pressButton (value) {
 
 function calculate() {
     let result = eval(display.innerText);
-    display.innerText = result;
+    display.innerText = parseFloat(result.toFixed(6));
 }
 
 function clearDisplay() {
@@ -37,73 +37,73 @@ function deleteLast() {
 
 function pressSqrt() {
   let result = Math.sqrt(eval(display.innerText));
-  display.innerText = result;
+  display.innerText = parseFloat(result.toFixed(6));
 }
 
 function pressLog() {
     let result = Math.log(eval(display.innerText));
-    display.innerText = result;
+    display.innerText = parseFloat(result.toFixed(6));
 }
 
 function pressLog10(){
    let result = Math.log10(eval(display.innerText));
-   display.innerText = result;
+   display.innerText = parseFloat(result.toFixed(6));
 }
 
 function pressSin() {
     let result = Math.sin(eval(display.innerText));
-    display.innerText = result;
+    display.innerText = parseFloat(result.toFixed(6));
 }
 
 function pressCos () {
     let result = Math.cos(eval(display.innerText));
-    display.innerText = result;
+    display.innerText = parseFloat(result.toFixed(6));
 }
 
 function pressTan() {
     let result = Math.tan(eval(display.innerText));
-   display.innerText = result;
+   display.innerText = parseFloat(result.toFixed(6));
 }
 
 function pressAcos() {
   let result = Math.acos(eval(display.innerText));
-  display.innerText = result;
+  display.innerText = parseFloat(result.toFixed(6));
 }
 
 function pressAsin() {
   let result = Math.asin(eval(display.innerText));
-  display.innerText = result;
+  display.innerText = parseFloat(result.toFixed(6));
 }
 
 function pressAtan() {
   let result = Math.atan(eval(display.innerText));
-  display.innerText = result;
+  display.innerText = parseFloat(result.toFixed(6));
 }
 
 function pressSinh() {
   let result = Math.sinh(eval(display.innerText));
-  display.innerText = result;
+  display.innerText = parseFloat(result.toFixed(6));
 }
 
 function pressCosh() {
   let result = Math.cosh(eval(display.innerText));
-  display.innerText = result;
+  display.innerText = parseFloat(result.toFixed(6));
 }
 
 function pressTanh() {
   let result = Math.tanh(eval(display.innerText));
-  display.innerText = result;
+  display.innerText = parseFloat(result.toFixed(6));
 }
 
 
 function pressAbs() {
   let result = Math.abs(eval(display.innerText));
-  display.innerText = result;
+  display.innerText = parseFloat(result.toFixed(6));
 }
 
 function pressMod() {
   let a=  eval(display.innerText.split("%")[0]);
   let b = eval(display.innerText.split("%")[1]);
-  display.innerText = a % b;
+  display.innerText = parseFloat((a % b).toFixed(6));
 }
 
